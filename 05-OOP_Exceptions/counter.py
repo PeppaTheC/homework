b6d81b7297d6f0455358255723eaca7394ce267b
PeppaTheC <52426305+PeppaTheC@users.noreply.github.com>
2019-11-25 18:00:44 +0300

    new_vision

--- 05-OOP_Exceptions/counter.py
+++ 05-OOP_Exceptions/counter.py
@@ -1,12 +1,14 @@
 def instances_counter(cls):
-
     class Wrapper(cls):
         counter = 0
 
-        def __new__(cls, *args, **kwargs):
-            cls.counter += 1
-            instance = super().__new__(cls)
-            return instance
+        # def __new__(cls, *args, **kwargs):
+        #     cls.counter += 1
+        #     instance = super().__new__(cls)
+        #     return instance
+
+        def __init__(self):
+            self.__class__.counter += 1
 
         @classmethod
         def get_created_instances(cls):
@@ -18,17 +20,19 @@ def instances_counter(cls):
             cls.counter = 0
             return counter
 
+    Wrapper.__name__ = cls.__name__
+    Wrapper.__doc__ = cls.__doc__
     return Wrapper
 
 
-# @instances_counter
+@instances_counter
 class User:
     pass
 
 
 if __name__ == '__main__':
-    User = instances_counter(User)
-    print(User.__name__)
+    # User = instances_counter(User)
+    # print(User.__name__)
     assert User.get_created_instances() == 0
     user, _, _ = User(), User(), User()
     assert user.get_created_instances() == 3
