d497f496220b14b3fd1274b31a060c47a80c83ac
PeppaTheC <52426305+PeppaTheC@users.noreply.github.com>
2019-11-26 11:55:32 +0300

    corrected

--- 05-OOP_Exceptions/counter.py
+++ 05-OOP_Exceptions/counter.py
@@ -27,7 +27,7 @@ class User:
 
 if __name__ == '__main__':
     assert User.get_created_instances() == 0
-    user = User()
-    assert user.get_created_instances() == 1
-    assert user.reset_instances_counter() == 1
+    user, _, _ = User(), User(),User()
+    assert user.get_created_instances() == 3
+    assert user.reset_instances_counter() == 3
     assert user.get_created_instances() == 0
