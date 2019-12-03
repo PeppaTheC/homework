2a8c7e0355c208ef27e98ae59481fa43268ef1ff
PeppaTheC <52426305+PeppaTheC@users.noreply.github.com>
2019-12-03 17:50:51 +0300

    docstrings and test structures

--- 07-python-for-advanced/siam_meta.py
+++ 07-python-for-advanced/siam_meta.py
@@ -1,12 +1,48 @@
+"""This module contains realization of  a metaclass SiamMeta.
+
+SiamMeta a special metaclass, class instances created using SiamMeta
+have the following properties:
+* objects created with the same attributes will be the same object
+* objects created with different attributes will be different objects
+* any object has the ability to access other objects same class
+through class attribute connect.
+
+    Typical usage example:
+
+    SiamObj(metaclass=SiamMeta):
+        pass
+"""
+
 import inspect
 import weakref
 
 
 class SiamMeta(type):
+    """ Metaclass SiamMeta.
+
+    SiamMeta gives the classes created on its basis the following properties:
+    * objects created with the same attributes will be the same object
+    * objects created with different attributes will be different objects
+    * any object has the ability to access other objects same class
+    through class attribute connect.
+
+    Attributes:
+        pool: A set of references on  unique classes instances.
+        connect: Method provides instances access other objects same class.
+    """
     _instances = {}
     pool = weakref.WeakSet()
 
     def __call__(cls, *args, **kwargs):
+        """Decorates class method __call__
+
+        Adds to class instances attributes pool and
+        method connect.
+
+        Returns:
+             New class instance if class attributes unique
+             otherwise return existed class instances with called attributes.
+        """
         params = inspect.signature(cls.__init__).bind_partial(cls, *args, **kwargs)
         params.apply_defaults()
         params = str(params)
@@ -21,6 +57,14 @@ class SiamMeta(type):
         return instance
 
     def connect(cls, *args, **kwargs):
+        """Provides instances access to other objects with same class.
+
+            Method provides  one class instances access to  another
+            instances of this class. Throw calling it created with attributes.
+
+            Returns:
+                Class instances with created attributes args and kwargs.
+        """
         params = inspect.signature(cls.__init__).bind_partial(cls, *args, **kwargs)
         params.apply_defaults()
         params = str(params)
@@ -28,6 +72,24 @@ class SiamMeta(type):
 
 
 class SiamObj(metaclass=SiamMeta):
+    """Example class
+
+    To use:
+    >>> unit1 = SiamObj('1', '2', a=1)
+    >>> unit2 = SiamObj('1', '2', a=1)
+    >>> unit1 is unit2
+    True
+    >>> unit3 = SiamObj('2', '2', a=1)
+    >>> unit3.connect('1', '2', 1).a = 2
+    >>> unit2.a == 2
+    True
+    >>> pool = unit3.pool
+    >>> print(len(pool))
+    2
+    >>> del unit3
+    >>> print(len(pool))
+    1
+    """
     def __init__(self, b: str, c: str, a: int):
         self.b = b
         self.c = c
--- 07-python-for-advanced/timer_property.py
+++ 07-python-for-advanced/timer_property.py
@@ -1,12 +1,46 @@
+"""Module provides realization of  a timer_property.
+
+Timer_property copies behavior of a built-in property with following extensions:
+* stores the result of the method for some time, which is transmitted
+parameter to initialize the property
+* recalculates value if timer expires
+
+    Typical usage example:
+    class Example:
+
+        @timer_property(2)
+        def msg(self):
+        return self._msg
+
+        @msg.setter
+        def msg(self, param):
+            self._msg = param
+"""
 import time
 import uuid
 
 
 def timer_property(timing: int = 0):
+    """Extend original property.
+
+    Args:
+      timing: Time in seconds for updating cache.
+
+    Returns:
+      Class Property.
+    """
     cache = {}
     start_time = {}
 
-    class Property(object):
+    class Property:
+        """Property with timing cache
+
+        Args:
+            fget: function to be used for getting an attribute value
+            fset: function to be used for setting an attribute value
+            fdel: function to be used for del'ing an attribute
+            doc: docstring
+        """
 
         def __init__(self, fget=None, fset=None, fdel=None, doc=None):
             self.fget = fget
@@ -17,10 +51,27 @@ def timer_property(timing: int = 0):
             self.__doc__ = doc
 
         def __set_name__(self, owner, name):
+            """Set new class attribute read as getter"""
             owner.read = self.getter(self.fget)
 
         def __get__(self, obj, objtype=None):
+            """Extension of original property get.
 
+            Replaces usual returning  by returning cache value
+            if timer didn't expire.
+
+            Args:
+                obj: Instance which tries to access to attribute .
+                objtype: Class of instance
+
+            Returns:
+                Result of method if timer didn't expired,
+                otherwise return cache
+
+            Raises:
+                AttributeError: An error occurred when function to be used
+                for getting an attribute value wasn't defined.
+            """
             if obj is None:
                 return self
             if self.fget is None:
@@ -33,6 +84,11 @@ def timer_property(timing: int = 0):
             return cache[obj]
 
         def __set__(self, obj, value):
+            """Extension of original property set.
+
+            Set an attribute of instance to value and
+            set new value into the cache and reset timer.
+            """
             if self.fset is None:
                 raise AttributeError("can't set attribute")
             nonlocal start_time, cache
@@ -41,20 +97,34 @@ def timer_property(timing: int = 0):
             return self.fset(obj, cache)
 
         def __delete__(self, obj):
+            """ Delete an attribute of instance. """
             if self.fdel is None:
                 raise AttributeError("can't delete attribute")
             self.fdel(obj)
 
         def getter(self, fget):
+            """ Descriptor to change the getter on a property."""
             return type(self)(fget, self.fset, self.fdel, self.__doc__)
 
         def setter(self, fset):
+            """ Descriptor to change the setter on a property."""
             return type(self)(self.fget, fset, self.fdel, self.__doc__)
 
     return Property
 
 
 class Message:
+    """Example class
+
+    To use:
+    >>> m = Message()
+    >>> initial = m.read
+    >>> initial is m.read
+    True
+    >>> time.sleep(2)
+    >>> initial is not m.read
+    False
+    """
 
     @timer_property(2)
     def msg(self):
@@ -66,15 +136,12 @@ class Message:
         self._msg = param
 
     def get_message(self):
-        """
-        Return random string
-        """
+        """ Return random string. """
         return uuid.uuid4().hex
 
 
 if __name__ == '__main__':
     m = Message()
-    a = Message()
     initial = m.read
     assert initial is m.read
     time.sleep(2)
