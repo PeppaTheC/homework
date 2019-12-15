97cfa4b2b77a3ff6049e4cf61ffcf22dca005cbe
Иван <ivan141096@yandex.ru>
2019-12-16 00:36:19 +0300

    bfs with several independent roots

--- 06-advanced-python/graph.py
+++ 06-advanced-python/graph.py
@@ -20,24 +20,25 @@ class GraphIterator:
 
     def __init__(self, source: Graph):
         self.graph = source.graph
-        self.visited, self.queue = [], deque(tuple(self.graph.keys())[0])
-
+        self.visited, self.queue = [], deque(tuple(self.graph.keys()))
         while self.queue:
             vertex = self.queue.popleft()
             for neighbour in self.graph[vertex]:
                 if neighbour not in self.visited:
                     self.visited.append(neighbour)
                     self.queue.append(neighbour)
+            if vertex not in self.visited:
+                self.visited.append(vertex)
 
     def __next__(self):
         try:
-            return self.visited.pop()
+            return self.visited.pop(0)
         except IndexError:
             raise StopIteration
 
 
 if __name__ == '__main__':
-    E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': ['E', 'G'], 'E': ['A'], 'G': [], 'D': ['A']}
+    E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': ['E', 'G'], 'E': [], 'G': [], 'D': [], 'K': ['O'], 'O': []}
     graph = Graph(E)
     for i in graph:
         print(i)
